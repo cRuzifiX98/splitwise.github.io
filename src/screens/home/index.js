527daaeaaddd6e254654855c6f3cfd5d90af0401
shanmuksrinu <shanmuksrinu@gmail.com>
2019-07-15 19:51:48 +0530

    UI navigation and friends Ui and groups UI added

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,45 +1,53 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StatusBar } from "react-native";
-import { Container, Button, H3, Text } from "native-base";
-
+// import { ImageBackground, View, StatusBar,StyleSheet } from "react-native";
+import {
+  Container, H3, Text, Header, TabHeading, Title,
+  Button,
+  Icon,
+  Left,
+  Right,
+  Body,
+  Tab,
+  Tabs
+} from "native-base";
+// import { Grid, Col } from "react-native-easy-grid";
 import styles from "./styles";
-
-const launchscreenBg = require("../../../assets/launchscreen-bg.png");
-const launchscreenLogo = require("../../../assets/logo-kitchen-sink.png");
+import HomeScreen from "../Friends";
+// import GroupsPage from "../Groups";
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        <StatusBar barStyle="light-content" />
-        <ImageBackground source={launchscreenBg} style={styles.imageContainer}>
-          <View style={styles.logoContainer}>
-            <ImageBackground source={launchscreenLogo} style={styles.logo} />
-          </View>
-          <View
-            style={{
-              alignItems: "center",
-              marginBottom: 50,
-              backgroundColor: "transparent"
-            }}
-          >
-            <H3 style={styles.text}>App to showcase</H3>
-            <View style={{ marginTop: 8 }} />
-            <H3 style={styles.text}>NativeBase components</H3>
-            <View style={{ marginTop: 8 }} />
-          </View>
-          <View style={{ marginBottom: 80 }}>
-            <Button
-              style={{ backgroundColor: "#6FAF98", alignSelf: "center" }}
-              onPress={() => this.props.navigation.openDrawer()}
-            >
-              <Text>Lets Go!</Text>
+        {/* <StatusBar barStyle="light-content" /> */}
+        <Header hasTabs>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.openDrawer()}>
+              <Icon name="menu" />
             </Button>
-          </View>
-        </ImageBackground>
+          </Left>
+          <Body>
+            <Title>Welcome to SplitWise</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Tabs>
+          <Tab heading={<TabHeading><Icon name="person" /><Text>Friends</Text></TabHeading>}>
+            <HomeScreen />
+          </Tab>
+          <Tab heading={<TabHeading><Icon name="people" /><Text>Group</Text></TabHeading>}>
+            {/* <GroupsPage /> */}
+            <HomeScreen />
+          </Tab>
+          <Tab heading={<TabHeading><Icon name="paper-plane" /><Text>Notifications</Text></TabHeading>}>
+            <H3 style={styles.text}>Hello world</H3>
+          </Tab>
+        </Tabs>
       </Container>
     );
   }
 }
+
 
 export default Home;
